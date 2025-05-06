77aaca8f09f600050f746acb61c763cef77511fa
yeonkimm <ky.kim@nation-a.com>
2025-05-06 17:04:08 +0900

    chore: css module import error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "@/styles/globals.css";
 import "@/styles/fonts/poppins.css";
 // core styles shared by all of react-notion-x (required)
 import "react-notion-x/src/styles.css";
-// used for code syntax highlighting (optional)
-import "prismjs/themes/prism-tomorrow.css";
 
 import type { AppProps } from "next/app";
 
